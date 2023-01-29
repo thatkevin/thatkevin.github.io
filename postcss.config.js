90954c15d0db065172b313b3b7f79c19cec303fd
Kevin Sedgley <kevin.sedgley@unboxedconsulting.com>
2023-01-29 23:06:42 +0000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("tailwindcss"),
+    require("tailwindcss")('./tailwind.config.js'),
     require("autoprefixer"),
     require("postcss-nested"),
-    ...(process.env.NODE_ENV == "production" ? [require("cssnano")({ preset: "default" })] : []),
   ],
 };
