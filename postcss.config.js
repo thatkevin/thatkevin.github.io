2b79e0788d881713ed45fa225aa7d58907d10d0b
Kevin Sedgley <kevin.sedgley@unboxedconsulting.com>
2023-01-31 08:10:41 +0000

    Dependancies

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,6 @@ module.exports = {
     require("tailwindcss")('./tailwind.config.js'),
     require("autoprefixer"),
     require("postcss-nested"),
+    ...(process.env.NODE_ENV == "production" ? [require("cssnano")({ preset: "default" })] : []),
   ],
 };
